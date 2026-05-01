11f870b056cc01468b94852a5daace780798511d
Z User <z@container>
2026-05-01 16:07:39 +0000

    إضافة basePath لـ GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/lms-platform",
   images: {
     unoptimized: true,
   },
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
